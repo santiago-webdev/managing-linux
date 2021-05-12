4a01b992bda16e0b9b0b2acd8d91c0ac33581fe4
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-05-11 22:04:36 -0300

    removed extra command

--- 10-baseinstall.sh
+++ 10-baseinstall.sh
@@ -115,5 +115,3 @@ echo -e "title\tArch Linux Zen\nlinux\t/vmlinuz-linux-zen\ninitrd\t/intel-ucode.
 echo -e "title\tArch Linux LTS\nlinux\t/vmlinuz-linux-lts\ninitrd\t/intel-ucode.img\ninitrd\t/initramfs-linux-lts.img\noptions cryptdevice=UUID=$(blkid -s UUID -o value $rootpartition):luks:allow-discards root=/dev/mapper/luks rootflags=subvol=@ rd.luks.options=discard rw" > /boot/loader/entries/arch-lts.conf
 
 echo -e "default\tarch.conf\ntimeout\t3\nconsole-mode max\neditor\tno" > /boot/loader/loader.conf
-
-umount -R /mnt
