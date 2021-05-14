817675f11bc3d9811b72e4ef1d24032b43b4b011
santiagogonzalezbogado <74136103+santiagogonzalezbogado@users.noreply.github.com>
2021-05-14 13:34:03 -0300

    Update 19-packages.sh

--- 19-packages.sh
+++ 19-packages.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir /datos
 sudo -s <<EOF
-echo -e "\n# /datos\nUUID=084c5be3-ad98-4203-ad97-44b68b483901\t/datos\t\text4\t\tdefaults\t0\t2"
+echo -e "\n# /datos\nUUID=084c5be3-ad98-4203-ad97-44b68b483901\t/datos\t\text4\t\tdefaults\t0\t2" >> /etc/fstab
 EOF
 
 sudo pacman -S rustup \
