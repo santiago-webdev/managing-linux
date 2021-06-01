01c44d631d423f1dd397689046b6b71b097d16a5
Santiago Gonzalez <74136103+santiagogonzalezbogado@users.noreply.github.com>
2021-06-01 16:33:01 -0300

    Update 11-baseunatended.sh

--- 11-baseunatended.sh
+++ 11-baseunatended.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 set -e
-encryption_passphrase=""
-hostname=""
-username=""
-read -p "Enter encryption passphrase: " encryption_passphrase
+
 read -p "Enter hostname: " hostname
 read -p "Enter username: " username
 read -p "Enter userpass: " user_password
@@ -12,14 +9,13 @@ read -p "Enter rootpass: " root_password
 continent_city="America/Argentina/Mendoza"
 
 timedatectl set-ntp true
-pacman -Sy --noconfirm
-sed -i 's/#UseSyslog/UseSyslog/g; s/#Color/Color/g; s/#TotalDownload/TotalDownload/g; s/#CheckSpace/CheckSpace/g' /etc/pacman.conf
+pacman -Sy
 
 sgdisk --zap-all /dev/nvme0n1
 printf "n\n1\n\n+333M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk /dev/nvme0n1 # ADD SWAP
 mkdir -p -m0700 /run/cryptsetup
-echo "$encryption_passphrase" | cryptsetup -q --align-payload=8192 -h sha512 -s 512 --use-random --type luks2 -c aes-xts-plain64 luksFormat /dev/nvme0n1p2 # SEE CRYPT OPTIONS
-echo "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 crypt_root # OR LUKS
+cryptsetup luksFormat /dev/nvme0n1p2
+cryptsetup luksOpen /dev/nvme0n1p2 crypt_root
 yes | mkfs.vfat -F32 /dev/nvme0n1p1
 yes | mkfs.btrfs /dev/mapper/crypt_root
 mount /dev/mapper/crypt_root /mnt
@@ -42,14 +38,14 @@ mount -o noatime,nodiratime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /de
 mount -o noatime,nodiratime,compress-force=zstd:1,space_cache=v2,subvolid=5 /dev/mapper/crypt_root /mnt/btrfs
 mount /dev/nvme0n1p1 /mnt/boot
 
-yes '' | pacstrap /mnt base base-devel linux linux-firmware \
-	intel-media-driver vulkan-intel libvdpau-va-gl \
+pacstrap /mnt base base-devel linux linux-firmware \
+	intel-media-driver vulkan-intel \
 	bluez bluez-utils tlp cryptsetup \
 	plasma konsole okular gwenview dolphin \
 	networkmanager efibootmgr intel-ucode neovim btrfs-progs wget
 
 genfstab -U /mnt >> /mnt/etc/fstab
-#arch-chroot /mnt /bin/bash << EOF
+arch-chroot /mnt /bin/bash << EOF
 timedatectl set-ntp true
 timedatectl set-timezone $continent_city
 hwclock --systohc --localtime
@@ -63,7 +59,6 @@ echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 echo "Defaults !tty_tickets" >> /etc/sudoers
 sed -i 's/-)/--threads=0 -)/g; s/gzip/pigz/g; s/bzip2/pbzip2/g; s/#MAKEFLAGS/MAKEFLAGS/g; s/-j2/-j$(nproc)/g' /etc/makepkg.conf
-sed -i 's/#UseSyslog/UseSyslog/g; s/#Color/Color/g; s/#TotalDownload/TotalDownload/g; s/#CheckSpace/CheckSpace/g' /etc/pacman.conf
 echo $hostname > /etc/hostname
 echo -en "$root_password\n$root_password" | passwd
 useradd -m -g users -G wheel,games,power,optical,storage,scanner,lp,audio,video,input,adm,users $username
