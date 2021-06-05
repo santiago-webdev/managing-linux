915cdbecca3e54d09e541a57157a90d42a79efe9
Santiago Gonzalez <74136103+santiagogonzalezbogado@users.noreply.github.com>
2021-06-05 17:22:30 -0300

    Removed preload since now I have only NVMe's

--- 20-packages.sh
+++ 20-packages.sh
@@ -114,7 +114,6 @@ if [[ "$packaur" = y ]]; then
 		zsh-fast-syntax-highlighting-git \
 		kwin-scripts-krohnkite-git \
 		auto-cpufreq-git \
-		preload \
 		grml-zsh-config \
 		
 else
@@ -125,7 +124,6 @@ systemctl enable sddm.service
 systemctl enable bluetooth.service
 #systemctl enable tlp.service
 #systemctl enable auto-cpufreq.service
-systemctl enable preload.service
 
 chsh -s /bin/zsh
 cd
