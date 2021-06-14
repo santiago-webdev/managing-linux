60544e5b4cc1ea846f387be830a0bdab19459852
Santiago Gonzalez <74136103+santiagogonzalezbogado@users.noreply.github.com>
2021-06-14 13:03:05 -0300

    made a more light plasma install

--- 36-configs.sh
+++ 36-configs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,display"
-sudo pacman -Rscn discover oxygen plasma-vault
 sed -i '/General/a \font=Inter, 14, -1,5,50,0,0,0,0,0' .config/krunnerrc
 kwriteconfig5 --file startkderc --group General --key systemdBoot true
 sudo journalctl --vacuum-size=100M
