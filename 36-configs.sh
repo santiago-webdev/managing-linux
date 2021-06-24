baa7e8ae97c3e63c7afad2b9a2e973df86b92fd4
Santiago Gonzalez <74136103+santiagogonzalezbogado@users.noreply.github.com>
2021-06-24 08:46:17 -0300

    Update 36-configs.sh

--- 36-configs.sh
+++ 36-configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 kwriteconfig5 --file kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,display"
-sed -i '/General/a \font=Inter, 15, -1,5,50,0,0,0,0,0' .config/krunnerrc
+sed -i '/General/a \font=Inter, 13, -1,5,50,0,0,0,0,0' .config/krunnerrc
 kwriteconfig5 --file startkderc --group General --key systemdBoot true
 sudo journalctl --vacuum-size=100M
 sudo journalctl --vacuum-time=2weeks
