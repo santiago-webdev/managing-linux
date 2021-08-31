375c3208eb7ecf1dd98743d4fa3774e6b8c75c85
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-08-31 11:28:13 -0300

    Install packer automatically for nvim

--- 50-nvim.sh
+++ 50-nvim.sh
@@ -7,4 +7,7 @@ git clone git@github.com:santigo-zero/Neovim.git .config/nvim
 pip install pynvim
 sudo npm i -g neovim
 
+git clone https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 rm $0 # Self delete
