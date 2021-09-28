ff138667b7207a99a65fe2c64a1ad5581b52a090
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-09-28 12:44:42 -0300

    Updates

--- 40-extra.sh
+++ 40-extra.sh
@@ -7,17 +7,11 @@ else
 	sudo pacman -S --noconfirm netbeans
 fi
 
-sudo chown $USER /data
-sudo chmod 750 /data
-sudo chown $USER /usr/share/wallpapers
-sudo chmod 750 /usr/share/wallpapers
-ln -s /data/wallpapers /usr/share/wallpapers
-ln -s /data/workspace ~/workspace
-mkdir -p ~/.local/share/kservices5
+# TODO add all the entries like this and not just delete them
+# xdg-user-dirs-update --set DOWNLOAD $HOME/.local/Downloads
+rm -rf .zshrc .bash_logout Documents Downloads Music Pictures Public Templates Videos paru
 
-rm -r .zshrc .bash_logout Documents Downloads Music Pictures Public Templates Videos paru
-
-firewall-cmd --zone=home --change-interface=wlp0s20f3 --permanent
-firewall-cmd --zone=home --add-service kdeconnect --permanent
+sudo firewall-cmd --zone=home --change-interface=wlp0s20f3 --permanent
+sudo firewall-cmd --zone=home --add-service kdeconnect --permanent
 
 rm $0 # Self delete
