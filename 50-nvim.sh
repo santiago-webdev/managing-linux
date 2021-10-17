9156396242c08ca2c9364254a794999ef3b9dda9
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-10-17 20:23:35 -0300

    Updated neovim script

--- 50-nvim.sh
+++ 50-nvim.sh
@@ -7,6 +7,11 @@ git clone git@github.com:santigo-zero/Neovim.git .config/nvim
 pip install pynvim
 sudo npm i -g neovim
 
+# Language Servers
+sudo npm i -g pyright						# Python
+sudo pacman -S bash-language-server			# Bash
+sudo pacman -S lua-language-server			# Lua
+
 git clone https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
