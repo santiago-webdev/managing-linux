db5900a54f842cb5a8b0970defcb695429d092e8
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-10 10:27:19 -0300

    arch-chroot should run correctly now

--- 10-installer.sh
+++ 10-installer.sh
@@ -42,12 +42,12 @@ if [[ $part == "no" ]]; then
     pacman -Sy dialog --noconfirm  # Install dialog for selecting disk
     devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
     drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
-    clear  # Clears blue screen from
-    lsblk  # Shows available drives
+    clear
+    lsblk
     echo "${drive}"  # Confirms drive selection
     part_boot="$(ls "${drive}"* | grep -E "^${drive}p?1$")"  # Finds boot partition
     part_root="$(ls "${drive}"* | grep -E "^${drive}p?2$")"  # Finds root partition
-    mkfs.vfat -F32 "${part_boot}"  # Format the EFI partition
+    mkfs.vfat -F32 "${part_boot}"
     echo -n "$drive_password" | cryptsetup open "${part_root}" cryptroot -d -  # Open the mapper
     mount /dev/mapper/cryptroot /mnt
 
@@ -78,13 +78,13 @@ if [[ $part == "no" ]]; then
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv /dev/mapper/cryptroot /mnt/srv
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /dev/mapper/cryptroot /mnt/tmp
     chattr +C /mnt/var  # Copy on write disabled
-    mount "${part_boot}" /mnt/boot  # Mount the boot partition
+    mount "${part_boot}" /mnt/boot
 else
     pacman -Sy dialog --noconfirm  # Install dialog for selecting disk
     devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
     drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
-    clear  # Clears blue screen from
-    lsblk  # Shows available drives
+    clear
+    lsblk
     echo "${drive}"  # Confirms drive selection
     sgdisk --zap-all "${drive}"  # Delete tables
     printf "n\n1\n\n+800M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk "${drive}"  # Format the drive
@@ -92,16 +92,16 @@ else
     part_boot="$(ls "${drive}"* | grep -E "^${drive}p?1$")"  # Finds boot partition
     part_root="$(ls "${drive}"* | grep -E "^${drive}p?2$")"  # Finds root partition
 
-    echo "${part_boot}"  # Confirms boot partition selection
-    echo "${part_root}"  # Confirms root partition selection
+    echo "${part_boot}"
+    echo "${part_root}"
 
-    mkdir -p /run/cryptsetup  # Change permission to root only
+    mkdir -p /run/cryptsetup
     chmod 0700 /run/cryptsetup /run
     echo -n "$drive_password" | cryptsetup luksFormat --type luks2 "${part_root}" -d -
     echo -n "$drive_password" | cryptsetup open "${part_root}" cryptroot -d -
 
-    mkfs.vfat -F32 "${part_boot}"  # Format the EFI partition
-    mkfs.btrfs /dev/mapper/cryptroot  # Format the encrypted partition
+    mkfs.vfat -F32 "${part_boot}"
+    mkfs.btrfs /dev/mapper/cryptroot
 
     mount /dev/mapper/cryptroot /mnt
     btrfs subvolume create /mnt/@
@@ -120,14 +120,14 @@ else
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv /dev/mapper/cryptroot /mnt/srv
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /dev/mapper/cryptroot /mnt/tmp
     chattr +C /mnt/var  # Copy on write disabled
-    mount "${part_boot}" /mnt/boot  # Mount the boot partition
+    mount "${part_boot}" /mnt/boot
 fi
 
-timedatectl set-ntp true  # Synchronize motherboard clock
+timedatectl set-ntp true
 
-sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
-sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf  # Parallel downloads
+sed -i "/#Color/a ILoveCandy" /etc/pacman.conf
+sed -i "s/#Color/Color/g" /etc/pacman.conf
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf
 
 read -p "Do you want to retrieve the latest mirrors?"$'\n' -n 1 -r
 echo
@@ -159,109 +159,109 @@ pacstrap -i /mnt base base-devel linux linux-firmware \
     networkmanager efibootmgr btrfs-progs zram-generator \
     ${cpu_model}
 
-genfstab -U /mnt >> /mnt/etc/fstab  # Generate the entries for fstab
-arch-chroot /mnt /bin/bash << EOF
+genfstab -U /mnt >> /mnt/etc/fstab
 
-ln -sf /usr/share/zoneinfo/"$(curl -s http://ip-api.com/line?fields=timezone)" /etc/localtime &>/dev/null
-hwclock --systohc
-sed -i "s/#en_US/en_US/g; s/#es_AR/es_AR/g" /etc/locale.gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-locale-gen
+# arch-chroot
+{
+  ln -sf /usr/share/zoneinfo/"$(curl -s http://ip-api.com/line?fields=timezone)" /etc/localtime &>/dev/null
+  hwclock --systohc
+  sed -i "s/#en_US/en_US/g; s/#es_AR/es_AR/g" /etc/locale.gen
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  locale-gen
 
-echo -e "127.0.0.1\tlocalhost" > /etc/hosts
-echo -e "::1\t\tlocalhost" >> /etc/hosts
-echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
+  echo -e "127.0.0.1\tlocalhost" > /etc/hosts
+  echo -e "::1\t\tlocalhost" >> /etc/hosts
+  echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
 
-echo -e "KEYMAP=$keymap" > /etc/vconsole.conf
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-echo "Defaults !tty_tickets" >> /etc/sudoers
-sed -i "/#Color/a ILoveCandy" /etc/pacman.conf
-sed -i "s/#Color/Color/g; s/#ParallelDownloads = 5/ParallelDownloads = 6/g; s/#UseSyslog/UseSyslog/g; s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
+  echo -e "KEYMAP=$keymap" > /etc/vconsole.conf
+  echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+  echo "Defaults !tty_tickets" >> /etc/sudoers
+  sed -i "/#Color/a ILoveCandy" /etc/pacman.conf
+  sed -i "s/#Color/Color/g; s/#ParallelDownloads = 5/ParallelDownloads = 6/g; s/#UseSyslog/UseSyslog/g; s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
 
-echo -e "$hostname" > /etc/hostname
-useradd -g users -G wheel -m $username
-echo -en "$root_password\n$root_password" | passwd
-echo -en "$user_password\n$user_password" | passwd $username
+  echo -e "$hostname" > /etc/hostname
+  useradd -g users -G wheel -m $username
+  echo -en "$root_password\n$root_password" | passwd
+  echo -en "$user_password\n$user_password" | passwd $username
 
-systemctl enable NetworkManager.service fstrim.timer
+  systemctl enable NetworkManager.service fstrim.timer
 
-journalctl --vacuum-size=100M --vacuum-time=2weeks
+  journalctl --vacuum-size=100M --vacuum-time=2weeks
 
-touch /etc/systemd/zram-generator.conf
-tee -a /etc/systemd/zram-generator.conf << END
-[zram0]
-zram-fraction = 1
-max-zram-size = 4096
-END
+  touch /etc/systemd/zram-generator.conf
+  tee -a /etc/systemd/zram-generator.conf << END
+  [zram0]
+  zram-fraction = 1
+  max-zram-size = 4096
+  END
 
-touch /etc/sysctl.d/99-swappiness.conf
-echo 'vm.swappiness=20' > /etc/sysctl.d/99-swappiness.conf
+  touch /etc/sysctl.d/99-swappiness.conf
+  echo 'vm.swappiness=20' > /etc/sysctl.d/99-swappiness.conf
 
-touch /etc/udev/rules.d/backlight.rules
-tee -a /etc/udev/rules.d/backlight.rules << END
-ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
-END
+  touch /etc/udev/rules.d/backlight.rules
+  tee -a /etc/udev/rules.d/backlight.rules << END
+  ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
+  END
 
-touch /etc/udev/rules.d/81-backlight.rules
-tee -a /etc/udev/rules.d/81-backlight.rules << END
-SUBSYSTEM=="backlight", ACTION=="add", KERNEL=="intel_backlight", ATTR{brightness}="9000"
-END
+  touch /etc/udev/rules.d/81-backlight.rules
+  tee -a /etc/udev/rules.d/81-backlight.rules << END
+  SUBSYSTEM=="backlight", ACTION=="add", KERNEL=="intel_backlight", ATTR{brightness}="9000"
+  END
 
-mkdir -p /etc/pacman.d/hooks/
-touch /etc/pacman.d/hooks/100-systemd-boot.hook
-tee -a /etc/pacman.d/hooks/100-systemd-boot.hook << END
-[Trigger]
-Type = Package
-Operation = Upgrade
-Target = systemd
+  mkdir -p /etc/pacman.d/hooks/
+  touch /etc/pacman.d/hooks/100-systemd-boot.hook
+  tee -a /etc/pacman.d/hooks/100-systemd-boot.hook << END
+  [Trigger]
+  Type = Package
+  Operation = Upgrade
+  Target = systemd
 
-[Action]
-Description = Updating systemd-boot
-When = PostTransaction
-Exec = /usr/bin/bootctl update
-END
+  [Action]
+  Description = Updating systemd-boot
+  When = PostTransaction
+  Exec = /usr/bin/bootctl update
+  END
 
-sed -i "s/^HOOKS.*/HOOKS=(base systemd keyboard autodetect sd-vconsole modconf block sd-encrypt btrfs filesystems fsck)/g" /etc/mkinitcpio.conf
-sed -i 's/^MODULES.*/MODULES=(intel_agp i915)/' /etc/mkinitcpio.conf
-mkinitcpio -P
-bootctl --path=/boot/ install
+  sed -i "s/^HOOKS.*/HOOKS=(base systemd keyboard autodetect sd-vconsole modconf block sd-encrypt btrfs filesystems fsck)/g" /etc/mkinitcpio.conf
+  sed -i 's/^MODULES.*/MODULES=(intel_agp i915)/' /etc/mkinitcpio.conf
+  mkinitcpio -P
+  bootctl --path=/boot/ install
 
-mkdir -p /boot/loader/
-tee -a /boot/loader/loader.conf << END
-default arch.conf
-console-mode max
-editor no
-END
+  mkdir -p /boot/loader/
+  tee -a /boot/loader/loader.conf << END
+  default arch.conf
+  console-mode max
+  editor no
+  END
 
-mkdir -p /boot/loader/entries/
-touch /boot/loader/entries/arch.conf
-tee -a /boot/loader/entries/arch.conf << END
-title Arch Linux
-linux /vmlinuz-linux
-initrd /$cpu_model.img
-initrd /initramfs-linux.img
-options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
-END
+  mkdir -p /boot/loader/entries/
+  touch /boot/loader/entries/arch.conf
+  tee -a /boot/loader/entries/arch.conf << END
+  title Arch Linux
+  linux /vmlinuz-linux
+  initrd /$cpu_model.img
+  initrd /initramfs-linux.img
+  options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
+  END
 
-touch /boot/loader/entries/arch-zen.conf
-tee -a /boot/loader/entries/arch-zen.conf << END
-title Arch Linux Zen
-linux /vmlinuz-linux-zen
-initrd /$cpu_model.img
-initrd /initramfs-linux-zen.img
-options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
-END
+  touch /boot/loader/entries/arch-zen.conf
+  tee -a /boot/loader/entries/arch-zen.conf << END
+  title Arch Linux Zen
+  linux /vmlinuz-linux-zen
+  initrd /$cpu_model.img
+  initrd /initramfs-linux-zen.img
+  options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
+  END
 
-touch /boot/loader/entries/arch-lts.conf
-tee -a /boot/loader/entries/arch-lts.conf << END
-title Arch Linux LTS
-linux /vmlinuz-linux-lts
-initrd /$cpu_model.img
-initrd /initramfs-linux-lts.img
-options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
-END
-
-EOF
+  touch /boot/loader/entries/arch-lts.conf
+  tee -a /boot/loader/entries/arch-lts.conf << END
+  title Arch Linux LTS
+  linux /vmlinuz-linux-lts
+  initrd /$cpu_model.img
+  initrd /initramfs-linux-lts.img
+  options lsm=landlock,lockdown,yama,apparmor,bpf rd.luks.name=$(blkid -s UUID -o value "${part_root}")=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard i915.fastboot=1 i915.enable_fbc=1 i915.enable_guc=2 nmi_watchdog=0 quiet rw
+  END
+} | arch-chroot /mnt
 
 read -n 1 -r -p "Do you wish to reboot? (y/N)"
 [[ $REPLY =~ ^[Yy]$ ]] && reboot
