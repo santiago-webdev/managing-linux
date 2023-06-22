a16bdb7e296e91191ca5e9034fc3919e4d811203
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-06-21 22:22:56 -0300

    Update config for VMs

--- 60-cockpit.sh
+++ 60-cockpit.sh
@@ -2,8 +2,29 @@
 
 # This script just installs cockpit on Arch Linux
 
-sudo pacman -S cockpit cockpit-machines cockpit-pcp cockpit-podman packagekit
+sudo pacman -S --needed \
+  cockpit \
+  cockpit-machines \
+  cockpit-pcp \
+  cockpit-podman \
+  iptables-nft \
+  dnsmasq \
+  openbsd-netcat \
+  libvirt \
 
-systemctl enable --now cockpit.socket
+if ! systemctl is-active --quiet cockpit.socket; then
+    systemctl enable --now cockpit.socket
+fi
 
-echo "Now you can connect to https://localhost:9090/"
+if ! systemctl is-active --quiet libvirtd.service; then
+    systemctl enable --now libvirtd.service
+fi
+
+# Check if user is part of the libvirt group
+if ! groups "$USER" | grep -q '\blibvirt\b'; then
+    sudo usermod -aG libvirt "$USER"
+    echo -e "User $USER has been added to the libvirt group"
+fi
+
+echo "Opening your browser at https://localhost:9090/"
+xdg-open "https://localhost:9090/"
